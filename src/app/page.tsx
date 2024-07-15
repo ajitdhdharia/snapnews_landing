9ba4f7bc72c9579a075ad6349bdac47eea1e3186
ajitdhdharia <ajitdhdhariaaa@gmail.com>
2024-07-15 19:15:56 -0400

    bg color change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { HeroParallaxPage } from "@/components/HeroParallaxPage";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-grid-small-black/[0.1]">
+    <main className="flex bg-white min-h-screen flex-col items-center justify-between p-24 bg-grid-small-black/[0.1]">
       <HeroParallaxPage />
     </main>
   );
